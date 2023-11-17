c598d17f3bc2184abeaf66b9b6465bc95df46712
Dobromir Kirov <kirov0407@gmail.com>
2023-11-17 19:47:25 +0200

    basic auth flow, jwt and cookie

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,5 +1,60 @@
-export const login = (req, res) => {};
+import { db } from "../connect.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { KEY } from "../config.js";
 
-export const register = (req, res) => {};
+export const login = (req, res) => {
+  const q = "SELECT * FROM users WHERE email = ?";
+  db.query(q, [req.body.email], (err, data) => {
+    if (err) return res.status(500).send(err);
+    if (data.length === 0) return res.status(404).send("User not found!");
 
-export const logout = (req, res) => {};
+    const checkPassword = bcrypt.compareSync(
+      req.body.password,
+      data[0].password
+    );
+
+    if (!checkPassword) return res.status(400).send("Wrong password or email!");
+
+    const token = jwt.sign({ id: data[0].id }, KEY);
+
+    const { password, ...rest } = data[0];
+
+    res
+      .cookie("accessToken", token, {
+        httpOnly: true,
+      })
+      .status(200)
+      .send(rest);
+  });
+};
+
+export const register = (req, res) => {
+  const q = "SELECT * FROM users WHERE username = ?";
+  db.query(q, [req.body.username], (err, data) => {
+    if (err) return res.status(500).send(err);
+    if (data.length) return res.status(409).send("User already registred!");
+
+    const salt = bcrypt.genSaltSync(10);
+    const hashedPassword = bcrypt.hashSync(req.body.password, salt);
+
+    const q = "INSERT INTO users (username, email, password, name) VALUE (?)";
+    const values = [
+      req.body.username,
+      req.body.email,
+      hashedPassword,
+      req.body.name,
+    ];
+    db.query(q, [values], (err, data) => {
+      if (err) return res.status(500).send(err);
+      return res.status(200).send("Succsessful registration");
+    });
+  });
+};
+
+export const logout = (req, res) => {
+  res
+    .clearCookie("accessToken", { secure: true, sameSite: "none" })
+    .status(200)
+    .send("Succsessful logout!");
+};
--- api/index.js
+++ api/index.js
@@ -1,17 +1,25 @@
-import Express from "express";
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
 import postRoutes from "./routes/posts.js";
 import commentRoutes from "./routes/comments.js";
 import likeRoutes from "./routes/likes.js";
 import chalk from "chalk";
 
-const app = Express();
+const app = express();
 const PORT = 8000;
+
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser())
 
 app.listen(PORT, () => {
   console.log(chalk.blue(`\n[SERVER LOG] API is running on port ${PORT}\n`));
 });
 
+app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/comments", commentRoutes);
