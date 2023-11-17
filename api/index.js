1d699ca1da5f3aadea192867246053e707a34880
Dobromir Kirov <kirov0407@gmail.com>
2023-11-17 16:27:29 +0200

    created initial user route and controller

--- api/index.js
+++ api/index.js
@@ -1,8 +1,12 @@
-import { Express } from "express";
+import Express from "express";
+import userRoutes from "./routes/users.js";
+import chalk from "chalk";
 
 const app = Express();
 const PORT = 8000;
 
 app.listen(PORT, () => {
-  console.log(`[SERVER LOG] API is running on port ${PORT}`);
+  console.log(chalk.blue(`\n[SERVER LOG] API is running on port ${PORT}\n`));
 });
+
+app.use("/api/users", userRoutes);
