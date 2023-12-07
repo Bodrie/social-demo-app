d28362c82ec2a3f23168ac54a2ab6482524d0525
Dobromir Kirov <kirov0407@gmail.com>
2023-12-07 21:56:22 +0200

    added sql query for getting pposts of current user and followed ones

--- api/controllers/post.js
+++ api/controllers/post.js
@@ -1,24 +1,38 @@
 import { db } from "../connect.js";
+import jwt from "jsonwebtoken";
+import { KEY } from "../config.js";
 
 export const getPost = (req, res) => {
-  const q = `SELECT 
-                p.id,
-                created_at AS createdAt,
-                user_id AS userId,
-                image AS contentImg,
-                description AS content,
-                u.name,
-                profile_picture AS profilePic
-            FROM posts AS p
-            JOIN users AS u
-            ON u.id = p.user_id`;
+  console.log(req.params);
+  const token = req.cookies.accessToken;
+  if (!token) return res.status(401).send("Not logged in!");
 
-  db.query(q, (err, data) => {
-    if (err) {
-      console.log("[SERVER LOG] Posts GET Error!");
-      console.log(err);
-      return res.status(500).send(err);
-    }
-    return res.status(200).send(data);
+  jwt.verify(token, KEY, (err, user) => {
+    if (err) return res.status(403).send("Invalid token!");
+
+    const q = `SELECT 
+                  p.id,
+                  created_at AS createdAt,
+                  user_id AS userId,
+                  image AS contentImg,
+                  description AS content,
+                  u.name,
+                  profile_picture AS profilePic
+              FROM posts AS p
+              JOIN users AS u
+              ON u.id = p.user_id
+              JOIN relationships AS r
+              ON p.user_id = r.followed_user_id
+              OR p.user_id = r.follower_user_id
+              AND r.follower_user_id = ?`;
+
+    db.query(q, [user.id], (err, data) => {
+      if (err) {
+        console.log("[SERVER LOG] Posts GET Error!");
+        console.log(err);
+        return res.status(500).send(err);
+      }
+      return res.status(200).send(data);
+    });
   });
 };
