2ee2db1223fadaf2a368c6dd78320481fff1bbc6
Dobromir Kirov <kirov0407@gmail.com>
2023-12-14 18:28:13 +0200

    fixed wrong server for the socket.io

--- client/src/socket.ts
+++ client/src/socket.ts
@@ -2,6 +2,7 @@
 import { io } from "socket.io-client";
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:10001';
+const SOCKET = process.env.REACT_APP_SOCKET;
+const URL = process.env.NODE_ENV === 'production' ? SOCKET : 'http://localhost:10001';
 
 export const socket = io(URL);
