d54ef5a00c64cd9c80d8c02a5aed838f610a0020
Dobromir Kirov <kirov0407@gmail.com>
2024-01-05 20:27:07 +0200

    created error msg in register form

--- client/src/pages/Register/Register.tsx
+++ client/src/pages/Register/Register.tsx
@@ -2,10 +2,19 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Register } from "../../types";
 import { register } from "../../services/axios";
+import { AxiosError, AxiosResponse } from "axios";
 import "./register.scss";
 
 const RegisterForm = () => {
   const navigate = useNavigate();
+
+  const [errors, setErrors] = useState<Register>({
+    username: "",
+    name: "",
+    email: "",
+    password: "",
+  });
+
   const [registerForm, setRegisterForm] = useState<Register>({
     username: "",
     name: "",
@@ -15,11 +24,19 @@ const RegisterForm = () => {
 
   const handleFormChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setRegisterForm((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+    setErrors((prev) => ({ ...prev, [e.target.name]: "" }));
   };
 
   const handleFormSubmit = (e: React.MouseEvent<HTMLButtonElement>) => {
     register(e, registerForm).then((res) => {
-      if (res.status === 200) navigate("/login");
+      const data = res as AxiosResponse<string>;
+      const error = res as AxiosError<Register>;
+      if (data.status === 200) {
+        navigate("/login");
+      } else if (error.response) {
+        const newErrorData = error.response.data;
+        setErrors(newErrorData);
+      }
     });
   };
 
@@ -29,34 +46,50 @@ const RegisterForm = () => {
         <div className="left">
           <h1>Register</h1>
           <form>
-            <input
-              type="text"
-              name="username"
-              placeholder="Name or nickname"
-              autoComplete="off"
-              onChange={handleFormChange}
-            />
-            <input
-              type="text"
-              name="name"
-              placeholder="Your real name"
-              autoComplete="off"
-              onChange={handleFormChange}
-            />
-            <input
-              type="email"
-              name="email"
-              placeholder="youremail@site.com"
-              autoComplete="off"
-              onChange={handleFormChange}
-            />
-            <input
-              type="password"
-              name="password"
-              placeholder="Password"
-              autoComplete="off"
-              onChange={handleFormChange}
-            />
+            <div className="input-container">
+              <input
+                className={`input ${errors.username && "error"}`}
+                type="text"
+                name="username"
+                placeholder="Name or nickname"
+                autoComplete="off"
+                onChange={handleFormChange}
+              />
+              <span className="error-msg">{errors.username}</span>
+            </div>
+            <div className="input-container">
+              <input
+                className={`input ${errors.name && "error"}`}
+                type="text"
+                name="name"
+                placeholder="Your real name"
+                autoComplete="off"
+                onChange={handleFormChange}
+              />
+              <span className="error-msg">{errors.name}</span>
+            </div>
+            <div className="input-container">
+              <input
+                className={`input ${errors.email && "error"}`}
+                type="email"
+                name="email"
+                placeholder="youremail@site.com"
+                autoComplete="off"
+                onChange={handleFormChange}
+              />
+              <span className="error-msg">{errors.email}</span>
+            </div>
+            <div className="input-container">
+              <input
+                className={`input ${errors.password && "error"}`}
+                type="password"
+                name="password"
+                placeholder="Password"
+                autoComplete="off"
+                onChange={handleFormChange}
+              />
+              <span className="error-msg">{errors.password}</span>
+            </div>
             <button onClick={handleFormSubmit}>Register</button>
           </form>
         </div>
