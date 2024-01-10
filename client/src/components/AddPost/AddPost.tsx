f445f429b316317bf9f88a6c5cb6cd9414c0c47d
Dobromir Kirov <kirov0407@gmail.com>
2024-01-10 21:32:37 +0200

    added package for emojis, with some animations

--- client/src/components/AddPost/AddPost.tsx
+++ client/src/components/AddPost/AddPost.tsx
@@ -1,17 +1,25 @@
 import React, { useContext, useState } from "react";
-import { AddAPhoto, PersonPin } from "@mui/icons-material";
+import {
+  AddAPhoto,
+  PersonPin,
+  SentimentSatisfiedAlt,
+} from "@mui/icons-material";
 import { AuthContext } from "../../context/authContext";
 import { addPost, upload, addActivity } from "../../services/axios";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { PostCreate } from "../../types/post";
+import EmojiPicker, { EmojiClickData, Theme } from "emoji-picker-react";
 import "./addPost.scss";
+import { ThemeContext } from "../../context/themeContext";
 
 const AddPost = () => {
   const queryClient = useQueryClient();
   const authCtx = useContext(AuthContext);
+  const themeCtx = useContext(ThemeContext);
   const [content, setContent] = useState("");
   const [file, setFile] = useState<File | null>(null);
   const [fit, setFit] = useState(false);
+  const [emojiPicker, setEmojiPicker] = useState(false);
 
   const mutation = useMutation({
     mutationFn: (newPost: PostCreate) => {
@@ -38,6 +46,11 @@ const AddPost = () => {
     let imgUrl = "";
     if (file) imgUrl = await upload(file);
     mutation.mutate({ content, contentImg: imgUrl });
+  };
+
+  const handleEmojiSelected = (emoji: EmojiClickData) => {
+    setEmojiPicker(false);
+    setContent((prevText) => prevText.concat(emoji.emoji));
   };
 
   return (
@@ -82,13 +95,36 @@ const AddPost = () => {
             setFile(e.currentTarget.files ? e.currentTarget.files[0] : null)
           }
         />
-        <label htmlFor="file">
+        <label htmlFor="file" className="interactions">
           <AddAPhoto />
           <span>Add photo</span>
         </label>
         <PersonPin />
         <span>Tag people</span>
-        <button onClick={handlePostShare}>Share</button>
+        <div
+          className="interactions"
+          onClick={() => setEmojiPicker(!emojiPicker)}
+        >
+          <SentimentSatisfiedAlt className="emoji-picker-icon" />
+
+          <div
+            className={`custom-emoji-picker ${emojiPicker ? "visible" : ""}`}
+            onClick={(e) => e.stopPropagation()}
+          >
+            <EmojiPicker
+              key={themeCtx?.theme}
+              lazyLoadEmojis={true}
+              onEmojiClick={handleEmojiSelected}
+              previewConfig={{ showPreview: false }}
+              searchDisabled={true}
+              theme={themeCtx?.theme === "dark" ? Theme.DARK : Theme.LIGHT}
+            />
+          </div>
+          <span>Emojis</span>
+        </div>
+        <button className="share" onClick={handlePostShare}>
+          Share
+        </button>
       </div>
     </div>
   );
