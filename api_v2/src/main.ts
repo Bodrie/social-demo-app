b09d3bd9d3c75c62bd334c0266f5fe3bd647e98e
Dobromir Kirov <dobromir.kirov@limechain.tech>
2024-10-19 10:43:53 +0300

    added logger service and clean up the basic env variables

--- api_v2/src/main.ts
+++ api_v2/src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { LoggerCall } from './common/services/logger.service';
 
 async function bootstrap() {
+  const PORT = process.env.PORT || 8080;
+  const ORIGINS = process.env.ORIGINS || 'http://localhost:3000';
   const app = await NestFactory.create(AppModule, {
-    cors: { origin: 'http://localhost:3000' },
+    cors: { origin: ORIGINS },
+    logger: new LoggerCall(),
   });
-  await app.listen(8080);
+  await app.listen(PORT);
 }
 bootstrap();
