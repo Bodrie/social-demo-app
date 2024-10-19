5a65342220dcf86444ae80d6cf518967072d114f
Dobromir Kirov <dobromir.kirov@limechain.tech>
2024-10-19 10:07:54 +0300

    git shinanigans...

---
+++ api_v2/src/auth/auth.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class AuthService {
+  // ADD ENV
+  private readonly saltRounds = 10;
+
+  async hashPassword(password: string): Promise<string> {
+    const salt = await bcrypt.genSalt(this.saltRounds);
+    return bcrypt.hash(password, salt);
+  }
+
+  async comparePasswords(
+    password: string,
+    hashedPassword: string,
+  ): Promise<boolean> {
+    return bcrypt.compare(password, hashedPassword);
+  }
+}
